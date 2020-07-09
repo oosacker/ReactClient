31c537464e9cdd84d8c3b566723b9f962830bda1
Natsuki Hasegawa <haseganats@ecs.vuw.ac.nz>
2020-07-09 17:15:22 +1200

    fixed the api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [apiResponse, setReponse] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:9000/testAPI")
+        .then(res => res.text())
+        .then(res => setReponse(res));
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Test API: {apiResponse}</p>
     </div>
   );
 }
